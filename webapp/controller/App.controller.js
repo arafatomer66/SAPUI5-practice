278961bf557becb734e1c881e0d3469293e36465
arafatomer66 <arafatomer66@gmail.com>
2023-09-27 15:22:19 +0600

    component configaration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,25 +9,6 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onInit: function () {
-        var oData = {
-          recipient: {
-            name: "Ui5",
-          },
-        };
-
-        var oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-
-        // set i18n model on view
-        var i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-        this.getView().setModel(i18nModel, "i18n");
-      },
-
       onShowHello: function () {
         // read msg from i18n model
         var oBundle = this.getView().getModel("i18n").getResourceBundle();
